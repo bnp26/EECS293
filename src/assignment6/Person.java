a9b0e9153026ccbbd49c2d2b86e22340de783922
boreh <bporeh@gmail.com>
2014-10-08 17:36:11 -0400

    changing the person mainly

--- src/assignment6/Person.java
+++ src/assignment6/Person.java
@@ -36,19 +36,84 @@ public class Person
     {
         int currentIndex = movesArray.indexOf(currentMoves);
         
-        Pillar currentPillar = currentMoves.peek();
+        Pillar mainPillar = currentMoves.peek();
         //gets the surrounding valid pillars
-        ArrayList<Pillar> surroundingPillars = getSurroundingPillars(maze, currentPillar.getRowNumber(), currentPillar.getColumnNumber());
+        ArrayList<Pillar> surroundingPillars = getSurroundingPillars(maze, mainPillar.getRowNumber(), mainPillar.getColumnNumber());
+        
+        
         
         for(int x = 0; x < surroundingPillars.size(); x++)
         {
+            //this is the current pillar pillar 
+            Pillar currentSurroundingPillar = surroundingPillars.get(x);
             Stack<Pillar> newMoves = new Stack();
             newMoves.addAll(currentMoves);
-            newMoves.push(surroundingPillars.get(x));
+            
+            newMoves.push(currentSurroundingPillar);
             movesArray.add(currentIndex+x, currentMoves);
         }
+    }
+    
+    //pre-condition: the two pillars are next to one another
+    private boolean hasPlank(Pillar pillar1, Pillar pillar2)
+    {
+        //pillar1's row number
+        int pillarOneRowNumber = pillar1.getRowNumber();
+        //pillar1's column number
+        int pillarOneColumnNumber = pillar1.getColumnNumber();
         
+        //pillar2's row number
+        int pillarTwoRowNumber = pillar2.getRowNumber();
+        //pillar2's column number
+        int pillarTwoColumnNumber = pillar2.getColumnNumber();
         
+        int plankLocation = getPlank(pillarOneRowNumber, pillarOneColumnNumber,
+                                     pillarTwoRowNumber, pillarTwoColumnNumber);
+        
+        return checkForPlank(pillar1, plankLocation);
+    }
+    
+    private boolean checkForPlank(Pillar pillar, int plankLocation)
+    {
+        boolean hasPlank;
+        switch(plankLocation)
+        {
+            case 0:
+                //checking left plank
+                hasPlank = pillar.getLeftPlank()!=Plank.NONE;
+                break;
+            case 1:
+                //checking right plank
+                hasPlank = pillar.getRightPlank()!=Plank.NONE;
+                break;
+            case 2:
+                //checking top plank
+                hasPlank = pillar.getTopPlank()!=Plank.NONE;
+                break; 
+            case 3:
+                //checking bottom plank
+                hasPlank = pillar.getBottomPlank()!=Plank.NONE;
+                break;
+            default:
+                hasPlank = false;
+        }
+        return hasPlank;
+    }
+    
+    private int getPlank(int row1, int col1, int row2, int col2)
+    {
+        int plankLocation;
+        if(row1 == row2)
+        {
+            //if column1 > column2 returns left. Otherwise, returns right.
+            plankLocation = (col1 > col2)?0:1;
+        }
+        else
+        {
+            //if row1 > row2 returns bottom. Otherwise, returns top
+            plankLocation = (row1 > row2)?3:2;
+        }
+        return plankLocation;
     }
     
     private Pillar getCurrentPillar(Maze maze, Point point)
